3220233fc41fdf06db7fa9efe00608eb3019df01
kimzerovirus <68390715+kimzerovirus@users.noreply.github.com>
2024-04-06 16:47:46 +0900

    Revert "add example with test"

--- src/main/java/io/sharing/passport/Passport.java
+++
@@ -1,9 +0,0 @@
-package io.sharing.passport;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface Passport {
-}
